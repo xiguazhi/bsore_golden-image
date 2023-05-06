46220026097e063108440b6eb20540198f3ae81f
svc-ansible <svc-ansible@localhost.localdomain>
2023-05-06 13:34:49 -0600

    Create packer build for vsphere images and ansible prov

--- common/vsphere-vm.tf
+++ common/vsphere-vm.tf
@@ -1,1 +1,10 @@
-`
+module "pipeline" {
+  source = "git@github.com:xiguazhi/terraform-vsphere.git"
+  environment      = "dev"
+  source           = "../modules/wtw-vm-linux/"
+  vmtemp           = "Ubuntu2004"
+  application_name = "pipeline"
+  vsphere_user     = var.vsphere_user
+  vsphere_password = var.vsphere_password
+  disk_size_gb     = ["60"]  
+}
