c7c186de21a4162e9f2cc62c0d60e6c37ecd1052
Brandon Sorenson <sorebran@isu.edu>
2023-05-06 17:21:11 -0600

    Fixing storage account terraform

--- common/storage-account.tf
+++ common/storage-account.tf
@@ -1,22 +1,9 @@
 data "azurerm_resource_group" "bsore-west2-hub" {
   name     = "bsore-WY-hub-rgrp"
-  location = "West Central US"
 }
 
 data "azurerm_storage_account" "hub-sa" {
   name     = "bsorewyhubsa"
-  location = data.azurerm_resource_group.bsore-west2-hub.location
-
-}
-
-data "azurerm_virtual_network" "hub-vnet" {
-  name     = "bsore-wy-hub-vnet"
-  location = data.azurerm_resource_group.bsore-west2-hub.location
-}
-
-data "azurerm_subnet" "mgmt_subn" {
-  name     = "bsore-wy-hub-subn"
-  location = data.azurerm_resource_group.bsore-west2-hub.location
 }
 
 resource "azurerm_storage_account_network_rules" "network_rules" {
