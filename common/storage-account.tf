aacc30f59acab62138f4e84e9de28f00d345dde8
Brandon Sorenson <sorebran@isu.edu>
2023-05-06 18:17:18 -0600

    noi subnets yet

--- common/storage-account.tf
+++ common/storage-account.tf
@@ -11,6 +11,5 @@ resource "azurerm_storage_account_network_rules" "network_rules" {
   storage_account_id         = data.azurerm_storage_account.hub-sa.id
   default_action             = "Deny"
   ip_rules                   = ["199.192.99.26"]
-  virtual_network_subnet_ids = [data.azurerm_subnet.mgmt_subn.id]
   bypass                     = ["Metrics", "Logging", "AzureServices"]
 }
