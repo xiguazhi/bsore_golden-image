9b4966e5984b4b131c65ec4563c7d2857a2cd64a
Brandon Sorenson <sorebran@isu.edu>
2023-05-06 22:31:18 -0600

    tttt

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,8 +1,8 @@
 variable "vsphere_user" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 variable "vsphere_password" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
